252f648259c617dceca843af73a78a9bc24ff264
ahmedfahimirfan <ahmed.bd2002@gmail.com>
2025-08-17 01:06:34 +0600

    First Commit

---
+++ src/StringCypher.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class StringCypher {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Message: ");
+        String str = sc.nextLine();
+        System.out.print("Enter the Key: ");
+        int key = sc.nextInt();
+        System.out.print("Encrypt? Write 'E'\nDecrypt? Write 'D'\n");
+        String str_input = sc.next();
+        if(str_input.equals("E"))
+        {
+            for (int i = 0; i <= str.length() - 1; i++)
+            {
+                int encrypt_ascii = (int) str.charAt(i) + key;
+                char encrypt_char = (char) encrypt_ascii;
+                System.out.print(encrypt_char);
+            }
+            System.out.println();
+        }
+        else if (str_input.equals("D"))
+        {
+            for (int i = 0; i <= str.length() - 1; i++)
+            {
+                int encrypt_ascii = (int) str.charAt(i) - key;
+                char encrypt_char = (char) encrypt_ascii;
+                System.out.print(encrypt_char);
+            }
+            System.out.println();
+        }
+    }
+}
